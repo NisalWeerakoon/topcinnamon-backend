c355b12505835900348b848fcde58b8d3018fcc4
NisalWeerakoon <nisalweerakoon07@gmail.com>
2025-09-19 13:26:24 +0530

    initialized springboot

---
+++ src/main/java/Project/CinnamonProducts/CinnamonProductsApplication.java
@@ -0,0 +1,13 @@
+package Project.CinnamonProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinnamonProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinnamonProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/CinnamonProducts/CinnamonProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.CinnamonProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinnamonProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
